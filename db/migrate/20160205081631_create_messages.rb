6901fa8767a4af78c006100edfe45f92d7e77157
tsuchimore1 <yoshi1003to216@gmail.com>
2016-02-06 08:06:17 +0000

    first commit

---
+++ db/migrate/20160205081631_create_messages.rb
@@ -0,0 +1,10 @@
+rake db:migrateclass CreateMessages < ActiveRecord::Migration
+  def change
+    create_table :messages do |t|
+      t.string :name
+      t.string :body
+
+      t.timestamps null: false
+    end
+  end
+end
